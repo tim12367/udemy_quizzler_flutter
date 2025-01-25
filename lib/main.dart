6eec6bfbd3fe567cbd9e05356bea647221754edd
tim12367 <tim12367@ymail.com>
2025-01-25 18:31:58 +0800

    1. fix warring 2. 按鈕按下 -> 新增下方圖示

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,11 +22,15 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
   @override
-  _QuizPageState createState() => _QuizPageState();
+  State<QuizPage> createState() => _QuizPageState();
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = []; // 下方紀錄
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -46,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
             ),
           ),
-        ),
+        ), // 問題
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -63,10 +69,16 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ));
+                });
               },
             ),
           ),
-        ),
+        ), // 按鈕是
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -83,11 +95,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  ));
+                });
               },
             ),
           ),
-        ),
-        //TODO: Add a Row here as your score keeper
+        ), // 按鈕否
+        Row(
+          children: scoreKeeper, // 下方紀錄
+        )
       ],
     );
   }
